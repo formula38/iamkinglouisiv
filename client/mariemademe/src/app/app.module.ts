0ab030d16b3b020c8900da4d290c96d7bd98830b
formula38 <louismconley@gmail.com>
2023-11-15 15:37:44 -0800

    adding current files

---
+++ client/mariemademe/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { GearComponent } from './gear/gear.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    PortfolioComponent,
+    GearComponent,
+    ContactComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ client/mariemademe/src/app/gear/gear.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gear',
+  templateUrl: './gear.component.html',
+  styleUrls: ['./gear.component.css']
+})
+export class GearComponent {
+
+}
