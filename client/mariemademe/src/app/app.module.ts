a9e027fff16b03400fcf1aa2a50b1edfd05ce58e
formula38 <louismconley@gmail.com>
2023-11-15 16:53:13 -0800

    adding current files

--- client/mariemademe/src/app/app.module.ts
+++ client/mariemademe/src/app/app.module.ts
@@ -3,22 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { GearComponent } from './gear/gear.component';
-import { ContactComponent } from './contact/contact.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
-    PortfolioComponent,
-    GearComponent,
-    ContactComponent,
-    HeaderComponent,
-    FooterComponent
   ],
   imports: [
     BrowserModule,
